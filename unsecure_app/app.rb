c8c2d72c650f7b6871e0654340602e8b13344758
Jose Lopez <joselopezgr@gmail.com>
2023-03-29 16:20:16 +0100

    ready for deployment

---
+++ unsecure_app/app.rb
@@ -0,0 +1,22 @@
+require 'sinatra/base'
+require "sinatra/reloader"
+
+class Application < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    return erb(:index)
+  end
+
+  post '/hello' do
+    @name = params[:name]
+    if @name.match?(/[!@£<>.$:""''%^&*()]/)
+      status 400
+      return ''
+    end
+    return erb(:hello)
+  end
+end
+
